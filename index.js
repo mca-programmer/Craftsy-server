5e9ed54224d36e20a51d2ea4ca8cc2ad1530e975
mca-programmer <freelancermca52@gmail.com>
2025-12-19 11:16:41 +0600

    add mongoDb

--- index.js
+++ index.js
@@ -10,6 +10,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.error("MONGODB_URI is not set in .env");
+  process.exit(1);
+}
+
+// MongoDB Client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+    // === Ping MongoDB ===
+    console.log("Connected to MongoDB! (crafty DB)");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+}
+run().catch(console.dir);
 
 // Start server
 app.listen(port, () => {
